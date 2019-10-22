fe89d6d1ebfea9c2bee8410b0a006ccf0f9b17c2
Virginia Dooley <vcdooley@gmail.com>
2019-10-22 14:34:56 +0100

    first commit, reusing previous project for data viz practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    current_timezone: 'London',
+    timezones_list: []
+  }
+
+  componentDidMount() {
+    this.getTimeZones()
+  }
+
+  getTimeZones = () => {
+      fetch('http://api.timezonedb.com/v2/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=Europe\/*')
+        .then(function(response) {
+          return response.json()
+        })
+        .then((response) => {
+          this.setState({
+            timezones_list: response.zones
+          })
+        })
+    }
+
+    renderTimeZones = () => {
+      let {timezones_list} = this.state
+      const list = timezones_list.map((timezone, index) => {
+        return (
+          <li key={index} onClick={() => this.onClick(timezone.zoneName)}>{timezone.zoneName}</li>
+        )
+      })
+      return list
+    }
+
+    onClick = (zone) => {
+
+      this.setState({
+        current_timezone: zone
+
+      })
+    }
+
+
+  render () {
+      console.log(this.state.timezones_list)
+    return (
+      <div>
+        {this.state.current_timezone}
+        <ol>
+          {this.renderTimeZones()}
+        </ol>
+      </div>
+    )
+
+  }
 }
 
 export default App;
